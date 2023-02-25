c98200c8c6309ddd7bc43bf240553e7f52ccdecb
anomic84@gmail.com <spencercarlson@Spencers-MacBook-Pro-2.local>
2023-02-24 19:27:28 -0800

    made handlebars, users with characters, login/logout boilerplate

---
+++ public/logout.js
@@ -0,0 +1,13 @@
+// Makes logout function
+const logoutFormHandler = async () => {
+
+    const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    });
+    document.location.replace('/login');
+};
+
+//Adds Event Listener to button with id #logout
+var logoutSubmit = document.querySelector('#logout')
+logoutSubmit.addEventListener('click', logoutFormHandler);
