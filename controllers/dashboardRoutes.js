b3548c740e48e12a1af7ed904e550c2a06d56287
Pearson <spearson@lwsd.org>
2023-02-26 12:42:27 -0800

    added homeroute, dashboardroute, index

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,51 @@
+const router = require('express').Router();
+const { Post } = require('../models/');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+    try {
+      const postData = await Post.findAll({
+        where: {
+            userId: req.session.userId,
+        }
+      });
+  
+      const posts = postData.map((post) => post.get({ plain: true }));
+  
+      res.render('allpostsadmin', {
+        layout: "dashboard", 
+        posts, 
+      });
+    } catch (err) {
+     res.redirect('login')
+    }
+  });
+
+  router.get('/new', withAuth, (req, res) => {
+    res.render('newpost', {
+        layout: 'dashboard',
+    })
+  })
+
+  router.get('/edit/:id', withAuth, async (req, res) => {
+    try {
+      const postData = await Post.findByPk(req.params.id);
+  
+      if (postData) {
+        const post = postData.get({ plain: true });
+  
+      res.render('editpost', {
+        layout: 'dashboard',
+        post,
+      });
+      } else {
+        res.status(404).end()
+      }
+  
+    } catch (err) {
+        res.redirect('login')
+    }
+  });
+
+
+module.exports = router
