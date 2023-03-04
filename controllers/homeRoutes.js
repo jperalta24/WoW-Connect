0b554c2eecfdd47042ca0e68f8131cc22f38e00d
Slender James <114044205+jperalta24@users.noreply.github.com>
2023-03-03 17:15:09 -0800

    added modals for new post and post replies

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -13,7 +13,7 @@ router.get('/', async (req, res) => {
       });
   
       const post = postData.map((post) => post.get({ plain: true }));
-      
+      console.log(post);
       res.render('homepage', { 
         post, loggedIn: req.session.loggedIn
       });
---
+++ public/main.js
@@ -0,0 +1,3 @@
+$('#respond-b').modal()
+console.log($('#respond-b'))
+
